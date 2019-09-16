a7479d816898f32174c7075bd0ca3ed2e27d870f
Thienhoangba99 <Thienhoangba99@users.noreply.github.com>
2019-09-16 11:17:41 +0700

    lap_1.1

---
+++ lab_1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab_1._1
+{
+    class BankAcoount
+    {
+        public int Id { get; private set; }
+        public int Balance { get; private set; }
+
+        private int id;
+        private int balance;
+
+        static void Main(string[] args)
+        {
+            BankAcoount acc = new BankAcoount();
+
+            acc.Id = 1;
+            acc.Balance = 15;
+
+            Console.WriteLine($"Account {acc.Id}, balance {acc.Balance}");
+        }
+    }
+}
